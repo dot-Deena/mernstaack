177f22266a6add35216cb6c47795f25066d27bf9
Deena Sreshta <deena@Deena.local>
2024-11-02 02:39:23 +0530

    nodejs5

--- APIs/productAPI.js
+++ APIs/productAPI.js
@@ -12,6 +12,24 @@ productApp.get('/products',async(req,res)=>{
     const productList = await product.find()
     res.send({message:"products",payload:productList})
 })
+// get (users by hash value whihc is obejct id)
+productApp.get('/products/:_id',async(req,res)=>{
+    try{
+        const Product = await product.findById(req.params._id)
+        res.send({message:"product", payload:Product})
+    }catch(err){
+        res.send({message:"error",payload:err.message})
+    }
+})
+// get user based on some field
+productApp.get('/product/:name',async(req,res)=>{
+    try{
+        const Product = await product.findOne({name:req.params.name})
+        res.send({message:"product", payload:Product})
+    }catch(err){
+        res.send({message:"error",payload:err.message})
+    }
+})
 // post 
 productApp.post('/product', async(req,res)=>{
     const newproduct = req.body // getting new user data
@@ -19,6 +37,16 @@ productApp.post('/product', async(req,res)=>{
     await pdoc.save() // save it
     res.send({message:"new product created"})
 
+})
+// update user
+productApp.put('/product', async(req,res)=>{
+    try{
+        const updatedproduct = req.body
+        let updateproduct =  await product.findOneAndUpdate({id:updatedproduct.id},{$set:{...updatedproduct}},{new:true})
+        res.send({message:"product", payload:updateproduct})
+    }catch(err){
+        res.send({message:"error",payload:err.message}) 
+    }
 })
 
 
--- APIs/userAPI.js
+++ APIs/userAPI.js
@@ -6,22 +6,55 @@ const user = require('../models/usermodel')
 userApp.use(exp.json())
 
 // operations
-// get
+// get (all users)
 userApp.get('/users',async(req,res)=>{
-
+    try{
       const userList = await user.find()
       res.send({message:"users",payload:userList})
-
+    }catch(err){
+        res.send({message:"error",payload:err.message})
+    } 
+})
+// get (users by hash value whihc is obejct id)
+userApp.get('/users/:_id',async(req,res)=>{
+    try{
+        const User = await user.findById(req.params._id)
+        res.send({message:"user", payload:User})
+    }catch(err){
+        res.send({message:"error",payload:err.message})
+    }
+})
+// get user based on some field
+userApp.get('/user/:name',async(req,res)=>{
+    try{
+        const User = await user.findOne({name:req.params.name})
+        res.send({message:"user", payload:User})
+    }catch(err){
+        res.send({message:"error",payload:err.message})
+    }
 })
 // post
 userApp.post('/user',async(req,res)=>{
+    try{
     const newuser = req.body
     // create user doctument
     const userdoc =  new user(newuser)
    // save to db
     await userdoc.save()
     res.send({message:"new user created"})
-
+    }catch(err){
+        res.send({message:"error",payload:err.message}) 
+    }
+})
+// update user
+userApp.put('/user', async(req,res)=>{
+    try{
+        const updatedUser = req.body
+        let updateuser =  await user.findOneAndUpdate({id:updatedUser.id},{$set:{...updatedUser}},{new:true})
+        res.send({message:"user", payload:updateuser})
+    }catch(err){
+        res.send({message:"error",payload:err.message}) 
+    }
 })
 
 
